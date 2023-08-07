d5985647fbfe8c53edf5ab0724c54a0d7a180740
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-08-08 00:31:14 +0400

    add functional «History of result»

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const secondInput = document.querySelector('.input-2');
 const operatorNode = document.querySelector('.select-operator');
 const resultBtn = document.querySelector('.result-btn');
 const output = document.querySelector('.output-value');
+const historyBox = document.querySelector('.history-result');
 
 function calc(a, b, operator) {
   switch(operator) {
@@ -14,7 +15,7 @@ function calc(a, b, operator) {
   }
 }
 
-form.addEventListener('submit', (e) => {
+function outputResult(e) {
   e.preventDefault();
 
   const num1 = Number(firstInput.value);
@@ -25,7 +26,26 @@ form.addEventListener('submit', (e) => {
 
   if(Number.isInteger(result)) {
     output.textContent = result;
+    addResultToHistory(result);
   } else {
     output.textContent = result.toFixed(2);
+    addResultToHistory(result.toFixed(2));
   }
-});
+}
+
+function addResultToHistory(res) {
+  const div = document.createElement('div');
+  div.classList.add('history-item');
+  div.textContent = res;
+  historyBox.append(div);
+}
+
+function deleteResultOfHistory(e) {
+  if(e.target.className === 'history-item') {
+    const elem = e.target;
+    elem.remove();
+  }
+}
+
+form.addEventListener('submit', (e) => outputResult(e));
+historyBox.addEventListener('click', (e) => deleteResultOfHistory(e));
